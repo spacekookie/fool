56e2c2099b53227396de27d56aca2b497b147d1d
Katharina Fey <kookie@spacekookie.de>
2018-01-03 16:33:13 +0100

    "code"

---
+++ src/git.rs
@@ -0,0 +1,20 @@
+//! A wrapper module around git commands
+//! 
+//! Every function that fool has access to should be conveniently wrapped here
+
+
+struct Git {
+
+}
+
+
+impl Git {
+
+    pub fn new(path: &str) -> Git {
+
+        
+
+        return Git {};
+    }
+
+}
