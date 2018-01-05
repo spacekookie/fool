4fc304acffcec17621ffb0cf063942e8806ab9a7
Katharina Fey <kookie@spacekookie.de>
2018-01-05 08:19:00 +0100

    Adding some basic UI stuff

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,86 @@
+extern crate cursive;
 
-extern crate ncurses;
 
-use ncurses::*;
+use cursive::Cursive;
+use cursive::event::{Event, Key};
+use cursive::traits::*;
+use cursive::views::{Dialog, Panel, OnEventView, TextArea, TextView, BoxView, Menubar};
+use cursive::menu::MenuTree;
+use cursive::align::*;
 
-static WINDOW_HEIGHT: i32 = 3;
-static WINDOW_WIDTH: i32 = 10;
+use cursive::theme::*;
 
 fn main() {
-    /* Setup ncurses. */
-    initscr();
-    raw();
+    let mut siv = Cursive::new();
+    siv.load_theme_file("assets/style.toml").unwrap();
 
-    /* Allow for extended keyboard (like F1). */
-    keypad(stdscr(), true);
-    noecho();
+    // The main dialog will just have a textarea.
+    // Its size expand automatically with the content.
+    // siv.add_layer(
+    //     Dialog::new()
+    //         .title("Describe your issue")
+    //         .padding((1, 1, 1, 0))
+    //         .content(TextArea::new().with_id("text"))
+    //         .button("Ok", Cursive::quit),
+    // );
 
-    /* Invisible cursor. */
-    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    let size = siv.screen_size();
+    let view = BoxView::with_fixed_size((size.x - 8, size.y - 4), Panel::new(TextArea::new().content(" Local:    master ~/Projects/code/fool
+ Head:     8ef7c41 Miep
 
-    /* Status/help info. */
-    printw("Use the arrow keys to move");
-    mvprintw(LINES() - 1, 0, "Press F1 to exit");
-    refresh();
 
-    /* Get the screen bounds. */
-    let mut max_x = 0;
-    let mut max_y = 0;
-    getmaxyx(stdscr(), &mut max_y, &mut max_x);
+ Changes:
+    Modified   Cargo.lock
+    Modified   Cargo.toml
+    Modified   src/main.rs
 
-    /* Start in the center. */
-    let mut start_y = (max_y - WINDOW_HEIGHT) / 2;
-    let mut start_x = (max_x - WINDOW_WIDTH) / 2;
-    let mut win = create_win(start_y, start_x);
+ # Cheat Sheet
+ #    s = stage file/section, S = stage all unstaged files
+ #    c = commit, C = commit -a (add unstaged)
+ #    P = push to upstream
+    ")));
+    siv.add_layer(view);
 
-    let mut ch = getch();
-    while ch != KEY_F(1) {
-        match ch {
-            KEY_LEFT => {
-                start_x -= 1;
-                destroy_win(win);
-                win = create_win(start_y, start_x);
-            }
-            KEY_RIGHT => {
-                start_x += 1;
-                destroy_win(win);
-                win = create_win(start_y, start_x);
-            }
-            KEY_UP => {
-                start_y -= 1;
-                destroy_win(win);
-                win = create_win(start_y, start_x);
-            }
-            KEY_DOWN => {
-                start_y += 1;
-                destroy_win(win);
-                win = create_win(start_y, start_x);
-            }
-            _ => {}
-        }
-        ch = getch();
-    }
+    // The menubar is a list of (label, menu tree) pairs.
+    siv.menubar()
+        .add_subtree("Help", MenuTree::new())
+        .add_subtree("Quit", MenuTree::new());
 
-    endwin();
+    siv.set_autohide_menu(false);
+    siv.add_global_callback(Key::Esc, |s| s.select_menubar());
+    siv.run();
 }
 
-fn create_win(start_y: i32, start_x: i32) -> WINDOW {
-    let win = newwin(WINDOW_HEIGHT, WINDOW_WIDTH, start_y, start_x);
-    box_(win, 0, 0);
-    wrefresh(win);
-    win
-}
+// Dialog::new()
+//     .title("Changes in the repo")
+//     .padding((1, 1, 1, 1))
+//     .h_align(HAlign::Center)
+//     .content(TextArea::new().content("--------------------------------------------------------"))
+//     .button("Ok", Cursive::quit),
 
-fn destroy_win(win: WINDOW) {
-    let ch = ' ' as chtype;
-    wborder(win, ch, ch, ch, ch, ch, ch, ch, ch);
-    wrefresh(win);
-    delwin(win);
-}
+// fn main() {
+//     // Read some long text from a file.
+//     let content = "Leverage agile frameworks to provide a robust
+//     synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further
+// the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.";
+//     let mut siv = Cursive::new();
+
+//     // We can quit by pressing q
+//     siv.add_global_callback('q', |s| s.quit());
+
+//     // The text is too long to fit on a line, so the view will wrap lines,
+//     // and will adapt to the terminal size.
+//     siv.add_layer(
+//         Dialog::around(Panel::new(TextView::new(content)))
+//             .h_align(HAlign::Center)
+//             .button("Quit", |s| s.quit())
+//             .full_screen(),
+//     );
+//     // Show a popup on top of the view.
+//     siv.add_layer(Dialog::info(
+//         "Try resizing the terminal!\n(Press 'q' to \
+//          quit when you're done.)",
+//     ));
+
+//     siv.run();
+// }
