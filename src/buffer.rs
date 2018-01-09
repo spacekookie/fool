b8c65d7cb5dca19d103db8b9eda33fb7a978e48d
Katharina Fey <kookie@spacekookie.de>
2018-01-09 03:07:26 +0100

    Adding selection logic

--- src/buffer.rs
+++ src/buffer.rs
@@ -11,13 +11,15 @@
 
 use std::fmt::{Write, Display, Formatter, Result};
 const CURSOR_CHAR: char = '█';
-const HELP_FOOTER: &'static str = "# Cheat Sheet\n
-#    s = stage file/section, S = stage all unstaged files\n
-#    c = commit, C = commit -a (add unstaged)\n
-#    P = push to upstream\n";
+// const HELP_FOOTER: &'static str = "# Cheat Sheet
+// #    s = stage file/section, S = stage all unstaged files
+// #    c = commit, C = commit -a (add unstaged)
+// #    P = push to upstream";
+const HELP_FOOTER: &'static str = "# Cheat Sheet (F1 for full help)
+#    s = stage file/section, c = commit, P = push to upstream, Q = quit";
 
 
-#[derive(Eq, PartialEq, Clone)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub enum ChangeType {
     // By default untracked or staged
     Added,
@@ -42,7 +44,7 @@ impl Display for ChangeType {
 
 pub struct Buffer {
     /// The selected position in the buffer
-    position: u64,
+    pub position: u64,
 
     /// Any file in the repo that is untracked
     untracked: Vec<(String, ChangeType)>,
@@ -136,8 +138,25 @@ impl Buffer {
     }
 
     pub fn move_down(&mut self) {
-        if self.position < 555 { // FIXME: Get actual length :P
+        if self.position < self.len()  as u64 - 1 { // FIXME: Why is the -1 required?
             self.position += 1;
+        }
+    }
+
+    /// Get the currently selected file that can then be 
+    pub fn get_selection(&self) -> (String, ChangeType) {
+        let untracked = self.untracked.len() as u64;
+        let unstaged = self.unstaged.len() as u64;
+
+        if self.position < untracked {
+            return get_element(&self.untracked, self.position);
+
+        } else if self.position >= untracked && self.position < untracked + unstaged {
+            // Over unstaged
+            return get_element(&self.unstaged, self.position - untracked);
+
+        } else {
+            return get_element(&self.staged, self.position - untracked - unstaged);
         }
     }
 
@@ -211,6 +230,10 @@ impl Buffer {
         return text;
     }
 
+    pub fn len(&self) -> usize {
+        return self.untracked.len() + self.unstaged.len() + self.staged.len();
+    }
+
     //         let mut res = String::new();
     // for (i, ch) in x.chars().enumerate() {
     //     write!(&mut res, "{} {}\n", i, ch).unwrap();
@@ -237,6 +260,19 @@ fn get_type(vec: &Vec<(String, ChangeType)>, item: &String) -> Option<ChangeType
     }
 
     return None;
+}
+
+fn get_element(vec: &Vec<(String, ChangeType)>, element: u64) -> (String, ChangeType) {
+    let mut ctr = 0;
+    for meh in vec {
+        if ctr == element {
+            return meh.clone();
+        }
+
+        ctr += 1;
+    }
+
+    return ("".to_owned(), ChangeType::Added);
 }
 
 /// A small utility function which counds the number of lines a string occupies
