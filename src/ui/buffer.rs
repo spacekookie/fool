265a747dd9a7121da496802f9e40c674f0d48aa6
Katharina Fey <kookie@spacekookie.de>
2018-01-27 15:51:11 +0100

    Adding outlines for new UI module

---
+++ src/ui/buffer.rs
@@ -0,0 +1,20 @@
+//! The text render buffer built on top of cursive
+//! 
+//! It handles a few things differently like overriding input handling to 
+//! manually scroll and manual text highlighting for certain words
+//! 
+//! The buffer is initialised with a GitBuffer object which represents the current state 
+//! of a git repository
+//! 
+//! ```notest
+//! let b = Buffer::new(git_buffer);
+//! ```
+
+
+pub struct Buffer {
+
+}
+
+impl Buffer {
+
+}
---
+++ src/ui/mod.rs
@@ -0,0 +1,9 @@
+//! A wrapper module around all UI elements in fool
+//! 
+//! 
+
+
+/// Represents the entire UI tree built for fool
+pub struct UI {
+
+}
