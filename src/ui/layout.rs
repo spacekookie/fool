673284274d31d4f38c2e93610ee097b01f103e29
Katharina Fey <kookie@spacekookie.de>
2018-02-06 21:59:34 +0100

    Using macros to do fence checking on the render loops

--- src/ui/layout.rs
+++ src/ui/layout.rs
@@ -11,10 +11,37 @@ use cursive::vec::Vec2;
 const CURSOR_CHAR: char = '█';
 const HELP_FOOTER: &'static str = "# Cheat Sheet
 #    s = stage, u = unstage, c = commit, P = push to upstream, Q = quit";
+const HELP_FOOTER_SIZE: usize = 2;
+
+macro_rules! check_y_bound {
+    ($curr:expr, $max:expr) => {
+        if $curr > $max {
+            break;
+        }
+    };
+}
+
+macro_rules! check_x_bound {
+    ($curr:expr, $max:expr) => {
+        ($curr < $max - HELP_FOOTER_SIZE)
+    };
+}
+
+
+pub struct LineSnippet {
+    line: String,
+    bounds: usize,
+}
+
+impl LineSnippet {
+    pub fn new(line: String, bounds: usize) -> LineSnippet {
+        return LineSnippet { line, bounds };
+    }
+}
 
 /// A layout represents items in a list on screen
 pub struct Layout {
-    text: String,
+    text: Vec<LineSnippet>,
     buf: Buffer,
     start: usize,
     stop: usize,
@@ -26,7 +53,7 @@ impl Layout {
             buf,
             start: 0,
             stop: res.y,
-            text: String::new(),
+            text: Vec::new(),
         };
 
         me.update(res);
@@ -34,7 +61,6 @@ impl Layout {
     }
 
     pub fn update(&mut self, res: Vec2) {
-
         /* Subtract the boundries around the text area */
         let mut res = res;
         res.x -= 2;
@@ -44,40 +70,80 @@ impl Layout {
         self.buf.update();
         self.text.clear();
 
-        let mut text = String::new();
+        let mut text = Vec::new();
         self.format_buffer(&mut text, res);
         self.text = text;
     }
 
     /// Format the buffer with only a read-only copy of the innards
-    fn format_buffer(&self, text: &mut String, res: Vec2) {
+    fn format_buffer(&self, text: &mut Vec<LineSnippet>, res: Vec2) {
         let mut y_pos = 0;
-        Layout::draw_line(text, (res.x, &mut y_pos), &format!("Remote:\t{}", &self.buf.remote));
-        Layout::draw_line(text, (res.x, &mut y_pos), &format!("Local:\t\t{}", &self.buf.local));
-        Layout::draw_line(text, (res.x, &mut y_pos), &format!("Head:\t\t{}", &self.buf.head));
-    }
 
-    /// Draws a single line of text into the text buffer
-    ///
-    /// Does boundry checking on how long the line can be as
-    /// well as if it should add padding
-    fn draw_line/*<S: Into<String>>*/(text_buffer: &mut String, res: (usize, &mut usize), line: &String) {
-        // let line: String = line.into();
-        let length = line.len();
-        let mut text: String = line.clone();
-        if length + 1 >= res.0 {
-            let slice = &line[..res.0 /* padding */ - 2]; // /* pad */ - 2 /* ... */ - 4
-            text = format!("{} ...", slice);
+        Layout::add_line(text, res.x, format!("Remote:  {}", &self.buf.remote));
+        Layout::add_line(text, res.x, format!("Local:   {}", &self.buf.local));
+        Layout::add_line(text, res.x, format!("Head:    {}", &self.buf.head));
+
+        /* Draw the Untracked block (if we have space) */
+        if self.buf.has_untracked() && check_x_bound!(y_pos, res.y) {
+            Layout::add_line(text, res.x, "");
+            y_pos = Layout::add_line(text, res.x, "Untracked files:");
+
+            for f in &self.buf.untracked {
+                check_y_bound!(y_pos, res.y);
+                y_pos = Layout::add_line(text, res.x, format!("  {}", &f.0));
+            }
         }
 
-        *res.1 += 1;
-        text.push_str("\n");
-        write!(text_buffer, "{}", &text).ok();
+        /* Draw the Changed block */
+        if self.buf.has_unstaged() && check_x_bound!(y_pos, res.y) {
+            Layout::add_line(text, res.x, "");
+            y_pos = Layout::add_line(text, res.x, "Changed files:");
+
+            for f in &self.buf.unstaged {
+                check_y_bound!(y_pos, res.y);
+                y_pos = Layout::add_line(text, res.x, format!("  {}", &f.0));
+            }
+        }
+
+        /* Draw the Staged block */
+        if self.buf.has_staged() && check_x_bound!(y_pos, res.y) {
+            Layout::add_line(text, res.x, "");
+            y_pos = Layout::add_line(text, res.x, "Staged files:");
+
+            for f in &self.buf.staged {
+                check_y_bound!(y_pos, res.y);
+                y_pos = Layout::add_line(text, res.x, format!("  {}", &f.0));
+            }
+        }
+    }
+
+    /// Adds a single line (with bounds) to the layout vector
+    /// 
+    /// Returns the new size of the vector that can be tracked externally
+    fn add_line<S: Into<String>>(text_buffer: &mut Vec<LineSnippet>, b: usize, line: S) -> usize {
+        let line: String = line.into();
+        let length = line.len();
+        let bounds = b - 4; // Subtract space for the " ..." at the end of the line
+        text_buffer.push(LineSnippet::new(line, bounds));
+        return text_buffer.len();
     }
 }
 
 impl Display for Layout {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        return write!(f, "{}", self.text);
+        let mut s = String::new();
+        for string in &self.text {
+            let length = string.line.len();
+            let mut tmp = string.line.clone();
+            if length + 1 >= string.bounds {
+                let slice = &string.line[..string.bounds];
+                tmp = format!("{} ...", slice);
+            }
+
+            s.push_str(&tmp);
+            s.push_str("\n");
+        }
+
+        return write!(f, "{}", s);
     }
 }
