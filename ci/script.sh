4d253bcdf399ffdd39e405cae1b6d4fa1edbaf22
Katharina Fey <kookie@spacekookie.de>
2018-02-08 12:27:33 +0100

    Trying to narrow down the issues to cross

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,15 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cargo build # --target $TARGET
+    cargo build # --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cargo test # --target $TARGET
+    cargo test # --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
